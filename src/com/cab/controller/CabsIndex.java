902982e9ae0d25da7ca9eab517ec5fd475b404c3
Risham <rishamdeep24@gmail.com>
2018-04-08 14:02:12 -0400

    BAMail functionality enabled

--- src/com/cab/controller/CabsIndex.java
+++ src/com/cab/controller/CabsIndex.java
@@ -26,12 +26,12 @@ import com.cab.controller.model.UserRegister;
 @Controller
 public class CabsIndex {
  
-//	@RequestMapping("/welcome")
-//	public ModelAndView helloWorld() {
-//		String message = "<br><div style='text-align:center;'>"
-//				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
-//		return new ModelAndView("welcome", "message", message);
-//	}
+	@RequestMapping("/welcome")
+public ModelAndView helloWorld() {
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
 	
 	
 	
