1eca0b934903e46c28abaedde3844e0fe97ae102
Risham <rishamdeep24@gmail.com>
2018-04-08 13:31:37 -0400

    Fix 3

--- src/com/cab/system/OTPGeneration.java
+++ src/com/cab/system/OTPGeneration.java
@@ -6,12 +6,12 @@ import java.util.Random;
 public class OTPGeneration {
 	
     public static String generatePassword() {
-        String chars = "1234567890";
+        String chars = "123456789";
 
         final int PW_LENGTH = 4;
         Random rnd = new SecureRandom();
         StringBuilder pass = new StringBuilder();
-        for (int i = 0; i < PW_LENGTH; i++)
+        for (int i = ;1 i < PW_LENGTH; i++)
             pass.append(chars.charAt(rnd.nextInt(chars.length())));
         return pass.toString();
     }
